396d9b388ce0e68d841b69aa9b0feb8bfa932fec
Denis Safronov <you@example.com>
2024-03-20 11:19:19 +0300

    Lab3_3_11

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import Head from 'next/head'
+import Image from 'next/image'
 import AwesomeSlider from "react-awesome-slider";
 import "react-awesome-slider/dist/styles.css";
 import "react-awesome-slider/dist/captioned.css";
 import styles from '../styles/Home.module.css'
+
+const buttonStyle = {
+    padding: "15px",
+    borderRadius: "50%",
+    background: "red",
+    opacity: 0.7,
+    fontSize: "20px"
+};
 
 const headerStyle = {
     color:'white',
@@ -55,20 +64,18 @@ function Animal(props){
 export default function Home() {
     const [animals,setAnimals] = React.useState([]);
     React.useEffect(()=>{
-        fetch('/public/animals.json').then(data=>data.json()).then(data=>setAnimals(data));
+        fetch('/animals.json').then(data=>data.json()).then(data=>setAnimals(data));
     },[]);
     React.useEffect(()=>{
-        if (typeof window !== 'undefined') {
-            let user = localStorage.getItem('user');
-            if (user===null){
-                while (user===null){
-                    user=prompt("Введите ваше имя пользователя");
-                    if (!user){
-                        alert('Обязательно!');
-                    }
-                    else {
-                        localStorage.setItem('user',user);
-                    }
+        let user = localStorage.getItem('user');
+        if (user===null){
+            while (user===null){
+                user=prompt("Введите ваше имя пользователя");
+                if (!user){
+                    alert('Обязательно!');
+                }
+                else {
+                    localStorage.setItem('user',user);
                 }
             }
         }
